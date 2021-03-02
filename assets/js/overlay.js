9f5668df21c098a09c5d3cb633b567f7bf383013
Andrea <git@andreaemmanuele.it>
2021-03-02 15:07:51 +0100

    Initial commit

---
+++ assets/js/overlay.js
@@ -0,0 +1,10 @@
+let overlay = document.getElementById("overlay");
+
+function ShowOverlay (id) {
+    overlay.style.display = "flex";
+
+    if (a == 1) {
+        let email = document.getElementById("email-container");
+        email.style.display = "flex";
+    }
+}
